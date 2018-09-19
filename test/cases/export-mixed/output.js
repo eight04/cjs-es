a6723859fe79031cb86d19e7c28a32d6bbb82b9c
eight <eight04@gmail.com>
2018-09-19 21:34:40 +0800

    Fix: export mixed

---
+++ test/cases/export-mixed/output.js
@@ -0,0 +1,3 @@
+const _module_exports_ = () => {};
+export default _module_exports_;
+_module_exports_.foo = "foo";
---
+++ test/cases/nested-module-unknown/output.js
@@ -0,0 +1,4 @@
+const _module_ = {exports: {}};
+_module_.exports = () => {};
+_module_.foo = "foo";
+export default _module_.exports;
